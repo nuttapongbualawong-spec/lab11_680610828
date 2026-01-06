3bc32b61429a1238d6ff47aee4f4638919e197ba
GARTIIE <nuttapong.bualawong@gmail.com>
2026-01-06 17:38:27 +0700

    lab11
    committed

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,25 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    srand(time(0));
+
+    string grades[] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+    
+    cout << "Press Enter 3 times to reveal your future.";
+
+    for(int i = 0; i < 3; i++) {
+        cin.get();
+    }
+
+    int randomIndex = rand() % 9;
+
+    cout << "You will get " << grades[randomIndex] << " in this 261102.";
+
+    return 0;
+}
